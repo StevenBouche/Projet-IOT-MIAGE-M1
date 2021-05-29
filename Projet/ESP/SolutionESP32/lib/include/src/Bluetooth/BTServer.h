41d9046d607baa365de7d7171297d15e4685f3fd
PierreGriseri <pierre.griseri@gmail.com>
2021-05-29 20:08:23 +0200

    #2 ESP Bluetooth

---
+++ Projet/ESP/SolutionESP32/lib/include/src/Bluetooth/BTServer.h
@@ -0,0 +1,97 @@
+#ifndef BTServer_h
+#define BTServer_h
+
+#include <Arduino.h>
+
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+#include <BLE2902.h>
+
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+typedef void (*OnMessageBluetooth)(String *message);
+
+class BTServer : public BLEServerCallbacks, public BLECharacteristicCallbacks{
+
+  private:
+    BLEServer *server;
+    BLEService *serviceGPS;
+    BLECharacteristic *characteristicGPS;
+    bool clientConnected;
+    OnMessageBluetooth onDataReceived;
+
+    void onConnect(BLEServer* pServer);
+    void onDisconnect(BLEServer* pServer);
+    void onWrite (BLECharacteristic * characteristic);
+
+  public:
+  
+    BTServer(){}
+    void setup(OnMessageBluetooth onDataReceived);
+
+};
+
+void BTServer::onConnect(BLEServer* pServer) {
+  this->clientConnected = true;
+  Serial.print("Event connection BLE");
+};
+ 
+void BTServer::onDisconnect(BLEServer* pServer) {
+  this->clientConnected = false;
+  Serial.print("Event disconnection BLE");
+}
+
+void BTServer::onWrite(BLECharacteristic *characteristic) {
+          
+  std::string rxValue = characteristic->getValue(); 
+
+  if (rxValue.length() > 0) {
+
+    Serial.print("Receive data from BLE");
+
+    String messageTemp;
+    for (int i = 0; i < rxValue.length(); i++) {
+      messageTemp += rxValue[i];
+    }
+    this->onDataReceived(&messageTemp);
+  }
+}
+
+void BTServer::setup(OnMessageBluetooth onDataReceived){
+
+   this->onDataReceived = onDataReceived;
+
+  Serial.println("Setup BLE Server.");
+
+  BLEDevice::init("ESP32-BLE");
+
+  this->server = BLEDevice::createServer();
+
+  this->server->setCallbacks(this);
+
+  this->serviceGPS = this->server->createService(SERVICE_UUID);
+
+  this->characteristicGPS = this->serviceGPS->createCharacteristic(
+    CHARACTERISTIC_UUID,
+    BLECharacteristic::PROPERTY_WRITE
+  );
+
+  this->characteristicGPS->setCallbacks(this);
+
+  this->serviceGPS->start();
+
+  // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
+  pAdvertising->setMinPreferred(0x12);
+  BLEDevice::startAdvertising();
+
+  Serial.println("Setup BLE Server finished.");
+
+}
+
+#endif
