b18c10ceea26ab0f9615f2de57273d9afa447336
steven <steven.bouche@outlook.com>
2021-05-29 20:10:42 +0200

    ajout wifi main

--- Projet/ESP/SolutionESP32/src/main.cpp
+++ Projet/ESP/SolutionESP32/src/main.cpp
@@ -1,5 +1,21 @@
 #include <Arduino.h>
+#include "Wifi/IdentifiantWifi.h"
+#include "Wifi/WifiESP.h"
 
+#include "Models/StateApp.h"
+
+//region variable_connection
+//  WiFi
+IdentifiantWifi ids[] = {
+  IdentifiantWifi("Bbox-4DD70ADE", "551F54E2D72A27CA1EA44567F149E1"),
+  IdentifiantWifi("iPhone de Steven", "iobwgn7obkf91")
+};
+WifiESP wifi;
+//endregion variable_connection
+
+//region variable_state_handler
+StateApp state;
+//endregion variable_state_handler
 
 void setup() {
 
