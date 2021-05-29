7bad5c4c190cc6f59c387de27d2b74a2cf2464b4
Armand Prévot <armand-prevot@hotmail.fr>
2021-05-29 21:47:59 +0200

    auth managment

---
+++ Projet/SolutionIOT/Projects/APIRobot/Controllers/AuthEquipmentController.cs
@@ -0,0 +1,37 @@
+﻿using APIRobot.Models;
+using APIRobot.Models.Auth;
+using APIRobot.Services;
+using Authentification.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIRobot.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthEquipmentController : ControllerBase
+    {
+
+        private readonly JwtService Service;
+
+        public AuthEquipmentController(JwtService Service)
+        {
+            this.Service = Service;
+        }
+
+        [DisableCors]
+        [AllowAnonymous]
+        [HttpPost("auth")]
+        public ActionResult<JwtToken> Login([FromBody] EquipmentAuthView equipment)
+        {
+
+            JwtToken token = Service.GetJwtToken(equipment);
+
+            if (token is null)
+                return BadRequest("{ \"reason\": \"Bad credencial\" }");
+
+            return Ok(token);
+        }
+    }
+}
