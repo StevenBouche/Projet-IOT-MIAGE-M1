f4e46be8f5ca4ffa7b2c715d8ea817b36ff25a7e
PierreGriseri <pierre.griseri@gmail.com>
2021-05-31 18:49:42 +0200

    #10 Mobile App avec Xamarin

---
+++ Projet/Mobile_App/TEST_XAMARIN/TEST_XAMARIN/ViewModels/MenuESPModel.cs
@@ -0,0 +1,20 @@
+﻿using Plugin.BLE.Abstractions.Contracts;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace TEST_XAMARIN.ViewModels
+{
+    public class MenuESPModel : BaseViewModel
+    {
+
+        public MenuESPModel()
+        {
+            Title = "Main Page";
+        }
+
+    }
+}
